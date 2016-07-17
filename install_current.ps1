99e5132ef4bee78a06a2e454dae52c357558e5ef
Joel Scoble <joel.scoble@outlook.com>
2016-07-17 14:47:33 -0500

    add powershell scripts

---
+++ install_current.ps1
@@ -0,0 +1,32 @@
+﻿# Install the current Go release
+
+# Setup the vars
+$workDir = "Documents\code\go"
+$workspace = Join-Path $Home $workDir
+$url = 'https://storage.googleapis.com/golang/go1.6.2.windows-amd64.msi'
+$output = Join-Path $Home "Downloads\go.msi"
+
+# Setup the workspace
+New-Item -path $workspace -type directory
+
+# Create GOPATH and set PATH to use $GOPATH\bin
+$workspaceBin = Join-Path $workspace "\bin"
+$workspaceBin = ';' + $workspaceBin
+[Environment]::SetEnvironmentVariable( "GOPATH", $workspace, [System.EnvironmentVariableTarget]::User ) 
+# Docs seem to say that: $env:Path + $workspaceBin should be used; but that results in repeating 
+# Path info; this seems to get the desired result.
+[Environment]::SetEnvironmentVariable( "Path", $workspaceBin, [System.EnvironmentVariableTarget]::User ) 
+
+# Create client, set its info, and download
+$wc = New-Object System.Net.WebClient
+$wc.UseDefaultCredentials = $true
+$wc.Headers.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f")
+$wc.DownloadFile($url, $output)
+
+Write-Output "$url downloaded as $output"
+
+# Run the msi
+Start-Process $output
+
+
+
---
+++ upgrade.ps1
@@ -0,0 +1,28 @@
+﻿# Url & destination
+$url = 'https://storage.googleapis.com/golang/go1.6.2.windows-amd64.zip'
+$output = Join-Path $Home "Downloads\go.zip"
+
+#Upgrade to current release of Go
+$bin = Join-Path $env:GOPATH bin\*
+$pkg = Join-Path $env:GOPATH pkg\*
+# Remove existing artifacts
+if (Test-Path $bin) {
+    Remove-Item $bin -recurse
+}
+if (Test-Path $pkg) {
+    Remove-Item $pkg -recurse
+}
+$goroot = [environment]::GetEnvironmentVariable("GOROOT", "Machine")
+$gorootAll = $goroot + "*"
+Remove-Item $gorootAll -recurse
+
+# Create client, set its info, and download
+$wc = New-Object System.Net.WebClient
+$wc.UseDefaultCredentials = $true
+$wc.Headers.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f")
+$wc.DownloadFile($url, $output)
+
+Write-Output "$url downloaded as $output"
+
+# Unzip the file
+Expand-Archive -Force -Path $output -DestinationPath $goroot\..
